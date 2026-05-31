73f8a88a181862d270919138bf382c6f90bbae60
Aleksandra Peskova <peskoval88@gmail.com>
2026-05-31 16:49:11 +0300

    +7

--- 7_exception2.py
+++ 7_exception2.py
@@ -18,7 +18,17 @@ def discounted(price, discount, max_discount=20):
     """
     Замените pass на ваш код
     """
-    pass
+    try:
+        price, discount, max_discount = float(price), float(discount), int(max_discount)
+    except (ValueError, TypeError):
+        print('Введеные некорректные данные.')
+    if max_discount >= 100:
+        raise ValueError('Максимальная скидка не должна быть больше 100')
+    if discount >= max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price - (price * discount / 100)
+    return price_with_discount
 
 
 if __name__ == "__main__":
