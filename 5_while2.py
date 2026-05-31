896db666f28d0c4f7281ec6da5a7e785a2e55dbc
Aleksandra Peskova <peskoval88@gmail.com>
2026-05-31 16:11:36 +0300

    +5

--- 5_while2.py
+++ 5_while2.py
@@ -15,14 +15,16 @@
 
 """
 
-questions_and_answers = {}
+questions_and_answers = {"Как дела": "Хорошо!", "Что делаешь?": "Программирую"}
 
 
 def ask_user(answers_dict):
     """
     Замените pass на ваш код
     """
-    pass
+    users_question = input("Enter your question: ")
+    while not questions_and_answers.get(users_question):
+        users_question = input("Enter your question: ")
 
 
 if __name__ == "__main__":
