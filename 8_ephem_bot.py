ae37bf554c06b2f7af88c5cc5ca54ea16b7e48aa
Aleksandra Peskova <peskoval88@gmail.com>
2026-06-03 19:13:16 +0300

    +8

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -22,13 +22,22 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     filename='bot.log')
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
+PLANETS = {
+    'Mercury': ephem.Mercury(),
+    'Venus': ephem.Venus(),
+    'Mars': ephem.Mars(),
+    'Jupiter': ephem.Jupiter(),
+    'Saturn': ephem.Saturn(),
+    'Uranus': ephem.Uranus(),
+    'Neptune': ephem.Neptune()
 }
+# PROXY = {
+#     'proxy_url': 'socks5://t1.learn.python.ru:1080',
+#     'urllib3_proxy_kwargs': {
+#         'username': 'learn',
+#         'password': 'python'
+#     }
+# }
 
 
 def greet_user(update, context):
@@ -44,11 +53,22 @@ def talk_to_me(update, context):
 
 
 def get_constellation(update, context):
-    user_text = update.message.text.split()[1].capitalize()
-    result = ephem.constellation(user_text, ephem.compute())
-    if result:
-        print(f'The {user_text} in {result[1]} constellation.')
+    user_text = update.message.text.split()
+    try: 
+        user_planet = user_text[1].capitalize()
+    except IndexError:
+        text = 'Where`s no planet to looking for or no such planet on my sky.'
+        user_planet = ''
 
+    if PLANETS.get(user_planet):
+        planet = PLANETS.get(user_planet)
+        planet.compute()
+        planet_data = ephem.constellation(planet)
+        text = f'The {user_planet} in {planet_data[1]} constellation.'
+    elif user_planet:
+        text = 'No such planet in my sky.'
+    update.message.reply_text(text)
+    
 
 def main():
     mybot = Updater(
